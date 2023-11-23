16b59d21a301d44683a591c834cd144c8a116646
Alisha875 <alishanayak001@gmail.com>
2023-11-23 16:19:34 +0530

    Student

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string fpath = "C:\\Users\\alish\\Desktop\\Mphasis\\Student TextFile\\student.txt";
+
+                string[] lines = File.ReadAllLines(fpath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error!!!" + ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
